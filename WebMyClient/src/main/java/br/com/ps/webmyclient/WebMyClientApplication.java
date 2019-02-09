c02a27f05a80aff7ab867422a9be9bd7e34345ca
Diego Santos <santos.diegomendonca@gmail.com>
2019-02-09 08:57:47 -0200

    primeiro commit

---
+++ WebMyClient/src/main/java/br/com/ps/webmyclient/WebMyClientApplication.java
@@ -0,0 +1,14 @@
+package br.com.ps.webmyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMyClientApplication.class, args);
+	}
+
+}
+
