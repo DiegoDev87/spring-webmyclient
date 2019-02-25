60f688573637227afe50e2f32a5f9a83e584599c
Diego Santos <diegosan.dev@gmail.com>
2019-02-25 14:26:31 -0300

    ajustes

--- WebMyClient/src/main/java/br/com/ps/webmyclient/usuario/UsuarioCadastroController.java
+++ WebMyClient/src/main/java/br/com/ps/webmyclient/usuario/UsuarioCadastroController.java
@@ -1,18 +1,14 @@
 package br.com.ps.webmyclient.usuario;
 
-import java.util.Arrays;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.ps.webmyclient.enums.Sexo;
 import br.com.ps.webmyclient.exception.EmailExisteException;
 
 @Controller
@@ -21,15 +17,6 @@ public class UsuarioCadastroController {
 
 	@Autowired
 	private UsuarioService usuarioService;
-
-	@GetMapping("/cadastro")
-	public ModelAndView cadastro() {
-		ModelAndView mav = new ModelAndView("/usuario/cadastro");
-		Usuario usuario = new Usuario();
-		mav.addObject("sexos", Arrays.asList(Sexo.values()));
-		mav.addObject("usuario", usuario);
-		return mav;
-	}
 
 	@PostMapping("/cadastro/salvar")
 	public ModelAndView salvar(@Valid Usuario usuario, BindingResult result) {
